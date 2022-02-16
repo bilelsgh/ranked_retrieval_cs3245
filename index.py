e52fb717b129e757f467d555cb57bcd32484beaa
bilelsgh <bilel.saghrouchni@gmail.com>
2022-02-16 17:30:21 +0800

    Build index for one doc without memory limits

--- index.py
+++ index.py
@@ -4,10 +4,17 @@ import nltk
 import os
 import sys
 import getopt
+import time
 from nltk.tokenize import wordpunct_tokenize
 from nltk import stem
 
-MEMORY = 100 #size ?
+"""
+Problems to fix :
+- 'involvement...wheth' as an unique token
+"""
+
+
+MEMORY = 100000 #size ?
 PUNCTUATION = [",",".",":","!",";","?","/",")","("]
 STEMMER = stem.PorterStemmer()
 
@@ -15,16 +22,23 @@ STEMMER = stem.PorterStemmer()
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
 
+def printDico(dic,post):
+    for key in list(dic.keys()):
+        print("> '{}' : \n   > #{} (size: {})".format(key,dic[key],len(post[dic[key]])))
+    print("========================\n")
 
-def SPIMIInvert(file):
-    dictionary = {}
+
+def SPIMIInvert(file,docID):
+    dictionary = {} # Format : {"token": postingListID, ..}
+    postingList = {} # Format : {postingListID1: { docID1: skipPointer,  docID2: -1 }, postingListID2: ... } -> skipPointer = a docID, -1 = no skippointer
     
     with open(file, 'r') as f:
         i = -1
+
         for line in f.readlines():
+            # == PREPROCESS STUFF ==
             stemmed_tokens_without_punct = []
             i +=1
-            print(line)
 
             #Tokenization
             for word in line.replace("\n","").split(" ") : # "Is U.S. big?" --> ["Is", "U.S.", "big?"] 
@@ -37,15 +51,31 @@ def SPIMIInvert(file):
                     stemmed_token = stemmed_token[:-1] if stemmed_token[-1] in PUNCTUATION else stemmed_token 
                     stemmed_tokens_without_punct.append(stemmed_token)
             
-            print(stemmed_tokens_without_punct)
             # finally  -> ["be", "u.s", "big"]
 
-            #Dictionary and posting list
+            # == SPIMI INVERT == 
             for token in stemmed_tokens_without_punct:
-                pass
-        return 0    
-            # tokens = wordpunct_tokenize(line)
-            # tokens_without_ponct = [token for token in tokens if token not in PUNCTUATION]
+                if len(dictionary) < MEMORY :
+                    #Is the token in the dictionary ?
+                    try:
+                        postingListID = dictionary[token]
+                        
+                        #We add the current docID to the posting list if it is not in yet
+                        try:
+                            postingList[postingListID][docID]
+                        except:
+                            postingList[postingListID][docID] = -1 
+                    except:
+                        dictionary[token] = list(dictionary.values())[-1]+1 if len(dictionary.values()) != 0 else 1
+                        postingList[dictionary[token]] = {}
+                        postingList[dictionary[token]][docID] = -1
+        
+        printDico(dictionary,postingList)
+        
+
+        #sort
+        #write
+
 
 
 def build_index(in_dir, out_dict, out_postings):
@@ -54,10 +84,12 @@ def build_index(in_dir, out_dict, out_postings):
     then output the dictionary file and postings file
     """
     print('indexing...')
-    
+    index = 0
     for filename in os.listdir("nltk_data/corpora/reuters/training/"):
-        SPIMIInvert(os.path.join("nltk_data/corpora/reuters/training/", filename))
-        return 0
+        SPIMIInvert(os.path.join("nltk_data/corpora/reuters/training/", filename),filename)
+        if index == 1:
+            return 0
+        index +=1 
 input_directory = output_file_dictionary = output_file_postings = None
 
 try:
