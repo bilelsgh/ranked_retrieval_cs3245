ff7444163257d7cb49a2a927bb35c4f7657f4655
CheeSengg <e0309953@u.nus.edu>
2022-02-20 15:43:19 +0800

    Added Shunting-Yard Algorithm & post-fix notation evaluation function

--- test.py
+++ test.py
@@ -1,33 +1,101 @@
-import re
 
-def find_brackets(li):
-    l = []
-    temp_str = ""
-    found_start = False
-    for i, item in enumerate(li):
-        if not found_start:
-            if item.find("(") != -1:
-                temp_str = item
-                found_start = True
-            else:
-                l.append(item)
-        else:
-            temp_str += " OR " + item
-            if item.find(")") != -1:
-                temp_str += " OR " + item
-                l.append(temp_str)
-                found_start = False
+from urllib.parse import quote_from_bytes
 
-    return l
 
-def order_opr(query):    
-    or_expr = query.split(" OR ")
-    or_expr = find_brackets(or_expr)
-    for i, expr in enumerate(or_expr):
-        or_expr[i] = expr.split(" AND ")
+def match_opr(token):
+    OPR = ["AND", "OR", "(", ")", "NOT"]
+    return token in OPR
+
+def search_documents(token):
+    if isinstance(token, list): ### tentatively list object (might change to sets for intersection/union fuction)
+        return token
+    
+    ### if it is not the stated object do a search through posting and dictionary (TBD)
     pass
 
+def eval_and(t1, t2):
+    t1 = search_documents(t1)
+    t2 = search_documents(t2)
 
-query = "bill OR Gates AND (vista OR XP) AND NOT mac"
-query = "bill OR Gates AND (vista OR XP)"
-order_opr(query)
+    return list(set(t1) & set(t2))
+
+def eval_or(t1, t2):
+    t1 = search_documents(t1)
+    t2 = search_documents(t2)
+
+    return list(set(t1) | set(t2))
+
+def eval_not(t):
+    t = search_documents(t)
+    ### should have a global with all the document id then we do a set difference
+    pass
+
+def eval_query(query):
+    l = []
+    for token in query:
+        if token == "NOT":
+            t = l.pop()
+            l.append(eval_not(t))
+        elif token == "AND":
+            t2 = l.pop()
+            t1 = l.pop()
+            l.append(eval_and(t1, t2))
+        elif token == "OR":
+            t2 = l.pop()
+            t1 = l.pop()
+            l.append(eval_or(t1, t2))
+        else: 
+            l.append(token)
+
+    return l[0]
+
+def query_processing(query):
+    query = query.replace("(", "( ")
+    query = query.replace(")", " )")
+    query = query.split(" ")
+    return query
+
+def generate_postfix_notation(query):
+    query = query_processing(query)
+    l = []
+    opr = []
+    for token in query: # is an operand
+        if match_opr(token):
+            if len(opr) == 0:
+                opr.append(token)
+            elif token == "OR":
+                if opr[-1] == "(":
+                    opr.append(token)
+                else:
+                    l.append(token)
+            elif token == "AND":
+                if opr[-1] == "OR":
+                    opr.append(token)
+                else:
+                    l.append(token)
+            elif token == "(":
+                opr.append(token)
+            elif token == ")":
+                while opr[-1] != "(":
+                    l.append(opr.pop())
+                opr.pop()
+                if opr[-1] == "NOT":
+                    l.append(opr.pop())
+            else: #token == "NOT"
+                opr.append(token)
+        else: # is a keywork
+            l.append(token)
+            if len(opr) != 0 and opr[-1] == "NOT":
+                l.append(opr.pop())
+
+    while len(opr) != 0:
+        l.append(opr.pop())
+
+    return l
+    
+
+query = "bill OR Gates AND NOT (vista OR XP) AND NOT mac"
+#query = "bill OR Gates AND (vista OR XP) AND (warren AND buffet)"
+query_order = generate_postfix_notation(query)
+print(query_order)
+#eval_query(query_order)
